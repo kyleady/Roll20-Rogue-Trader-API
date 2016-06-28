578a0132d5a9282414c71a0d21f5192226262dce
kyleady <kyledavidady@gmail.com>
2016-06-28 02:31:04 -0700

    Fate Cleanup
    Used a surer method to detect when the user is a GM
    playerIsGM(playerid)
    
    I removed some old testing features from the code that were no longer
    needed.
    
    Used _.each for both !Fate and !FateReset. See below for reasons.
    
    !Fate: Though it is unlikely that a player will have multiple characters
    and attempt to spend a Fate Point for each of them, it is better to
    allow for this then for them to unknowingly select multiple characters
    and keep spending a Fate Point on the wrong character (whichever one the
    API decides is character[0]).
    
    !FateReset: Previously the code searched through every single character
    in the library and reset their Fate Points. My library has grown quite
    large and this function became too inconvenient to use. By only reseting
    the Fate Points for those characters selected, the processing time is
    dramatically reduced.

--- Fate.js
+++ Fate.js
@@ -2,63 +2,84 @@ on("chat:message", function(msg) {log(msg)});
 on("chat:message", function(msg) {
     //Fate - spends a fate point if it is available
   if(msg.type == "api" && msg.content == ("!Fate")) {
-      //be sure the sender has a represented token selected
-      // && msg.selected[0].represents != undefined
+    //be sure the sender has at least one token selected
     if(msg.selected){
-        //find the character the token represents
-        var graphic = getObj("graphic", msg.selected[0]._id);
-        //be sure the graphic is valid
-        if(graphic == undefined){
-            sendChat(msg.who, "/em - graphic undefined.") 
-            return;
-        }
-        //be sure the character is valid
-        var character = getObj("character",graphic.get("represents"))
-        if(character == undefined){
-            sendChat(msg.who, "/em - character undefined.") 
-            return;
-        }
-        
-        //if the character has Fate and it is above zero...
-        if(getAttrByName(character.id, "Fate") && getAttrByName(character.id, "Fate") > 0)
-        {
-            //Find the Fate attribute on the selected Character
-            var attribObj = findObjs({ type: 'attribute', characterid: character.id, name: "Fate" })[0];
-            //reduce the fate points by one
-            var Fate = attribObj.get('current') -1;
-            attribObj.set('current', Fate);
-            //tell everyone that msg.who used a fate point and how many fate points they have left
-            sendChat(msg.who, "/em - Fate Point used. " + getAttrByName(character.id, "Fate") + " Fate Points remain.")
-        }
-        else //otherwise the Character cannot spend fatepoints
-        {
-            sendChat(msg.who, "/em - The Emperor does not love you. No fate point is expended.")
-        }
+		//make each token spend a fate point if it can
+		_.each(msg.selected,function(obj){
+			//find the character the token represents
+			var graphic = getObj("graphic", obj._id);
+			//be sure the graphic is valid
+			if(graphic == undefined){
+				//have the player tell the GM what went wrong
+				sendChat(msg.who, "/w gm !Fate: graphic undefined.");
+				return;
+			}
+			//be sure the character is valid
+			var character = getObj("character",graphic.get("represents"))
+			if(character == undefined){
+				//have the player tell the GM what went wrong
+				sendChat(msg.who, "/w gm !Fate: character undefined.");
+				return;
+			}
+			
+			//if the character has Fate and it is above zero...
+			if(getAttrByName(character.id, "Fate") && getAttrByName(character.id, "Fate") > 0)
+			{
+				//Find the Fate attribute on the selected Character
+				var attribObj = findObjs({ type: 'attribute', characterid: character.id, name: "Fate" })[0];
+				//reduce the fate points by one
+				var Fate = attribObj.get('current') -1;
+				attribObj.set('current', Fate);
+				//tell everyone that msg.who used a fate point and how many fate points they have left
+				sendChat(msg.who, "/em - Fate Point spent. " + getAttrByName(character.id, "Fate") + " Fate Points remain for " + character.get("name") + ".");
+			}
+			else //otherwise the Character cannot spend fatepoints
+			{
+				sendChat(msg.who, "/em - The Emperor does not love you. " + character.get("name")  + " has no Fate Points to spend.");
+			}
+		});
+	}
+	else {
+		
+		sendChat(msg.who, "/w gm !Fate: Nothing selected.");
+	}	
+  }  
+  
+//FateReset - Resets the selected characters' Fate Points back to their maximum
+//only the GM may actiavate this function
+else if(msg.type == "api" && msg.content == ("!FateReset") && playerIsGM(msg.playerid)){
+	//be sure the sender has at least one token selected
+    if(msg.selected){
+		//make each token spend a fate point if it can
+		_.each(msg.selected,function(obj){
+			//find the character the token represents
+			var graphic = getObj("graphic", obj._id);
+			//be sure the graphic is valid
+			if(graphic == undefined){
+				//have the player tell the GM what went wrong
+				sendChat(msg.who, "/w gm !Fate: graphic undefined.");
+				return;
+			}
+			//be sure the character is valid
+			var character = getObj("character",graphic.get("represents"))
+			if(character == undefined){
+				//have the player tell the GM what went wrong
+				sendChat(msg.who, "/w gm !Fate: character undefined.");
+				return;
+			}
+			
+			//only reset Fate, if the character has Fate
+			if(getAttrByName(character.id, "Fate")){
+				//get an editable version of the Fate Attribute from the Character
+				var attribObj = findObjs({ type: 'attribute', characterid: character.id, name: "Fate" })[0];
+				
+				//set each character's fate back to its maxiumum
+				attribObj.set('current', attribObj.get('max'));
+				
+				//inform the gm that the function has completed successfully for this character
+				sendChat(msg.who,"/w gm Fate has been reset: " + character.get("name") );
+			}
+        });
     }
-    else {
-        
-        sendChat(msg.who, "/em - Please select a character.")
-    }
-  }  //FateReset - Resets every character's Fate Points back to their maximum
-     //only the GM may actiavate this function
-  else if(msg.type == "api" && msg.content == ("!FateReset") && msg.who.indexOf(" (GM)") !== -1)
-  {
-    //create a list of every single character in the campaign  
-    var characters = findObjs({ type: 'character'});
-    for (i = 0; i < characters.length; i++) 
-    {
-        //for every single character, find their Fate Attribute
-        if(findObjs({ type: 'attribute', characterid: characters[i].id, name: "Fate" }).length > 0)
-        {
-            var attribObj = findObjs({ type: 'attribute', characterid: characters[i].id, name: "Fate" })[0];
-            //set each character's fate back to its maxiumum
-            var Fate = attribObj.get('max');
-            attribObj.set('current', Fate);
-        }
-    }
-    //inform the gm that the function has completed successfully
-    sendChat(msg.who,"/w gm Fate has been reset.")
-  } else if(msg.type == "api" && msg.content.indexOf("!Output ") == 0){
-      sendChat("System","/w gm " + msg.content.substring(8));
-  }
+}
 });
