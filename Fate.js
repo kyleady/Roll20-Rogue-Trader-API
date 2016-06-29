80fcf61a8fb416417c4f9289bbaa837a2a1235b7
kyleady <kyledavidady@gmail.com>
2016-06-28 17:40:33 -0700

    Added Alert
    Previously if the gm selected nothing or just invalid tokens, nothing
    would happen and the function would not announce anything. The gm might
    wait for it to finish processing. This alert tells the gm when the
    function finishes processing if no valid tokens were selected.

--- Fate.js
+++ Fate.js
@@ -25,12 +25,12 @@ on("chat:message", function(msg) {
 			//if the character has Fate and it is above zero...
 			if(getAttrByName(character.id, "Fate") && getAttrByName(character.id, "Fate") > 0)
 			{
-				//Find the Fate attribute on the selected Character
+				//get an editable version of the Fate Attribute from the Character
 				var attribObj = findObjs({ type: 'attribute', characterid: character.id, name: "Fate" })[0];
 				//reduce the fate points by one
 				var Fate = attribObj.get('current') -1;
 				attribObj.set('current', Fate);
-				//tell everyone that msg.who used a fate point and how many fate points they have left
+				//tell everyone that msg.who used a fate point and how many fate points the selected character has left.
 				sendChat(msg.who, "/em - Fate Point spent. " + getAttrByName(character.id, "Fate") + " Fate Points remain for " + character.get("name") + ".");
 			}
 			else //otherwise the Character cannot spend fatepoints
@@ -40,7 +40,7 @@ on("chat:message", function(msg) {
 		});
 	}
 	else {
-		
+		//alert the gm that the player tried to spend Fate Points while having no one selected
 		sendChat(msg.who, "/w gm !Fate: Nothing selected.");
 	}	
   }  
@@ -48,6 +48,8 @@ on("chat:message", function(msg) {
 //FateReset - Resets the selected characters' Fate Points back to their maximum
 //only the GM may actiavate this function
 else if(msg.type == "api" && msg.content == ("!FateReset") && playerIsGM(msg.playerid)){
+	//be ready to alert the gm if no characters had their fate points restored
+	var NothingAccomplished = true;
 	//be sure the sender has at least one token selected
     if(msg.selected){
 		//make each token spend a fate point if it can
@@ -78,8 +80,15 @@ else if(msg.type == "api" && msg.content == ("!FateReset") && playerIsGM(msg.pla
 				
 				//inform the gm that the function has completed successfully for this character
 				sendChat(msg.who,"/w gm Fate has been reset: " + character.get("name") );
+				
+				//record that we were able to restore the Fate Points of at least one character
+				NothingAccomplished = false;
 			}
-        });
-    }
+		});
+	}
+	//if we reached the end of FateReset and nothing was accomplished, you should let the gm know
+	if(NothingAccomplished){
+		sendChat("System", "/w gm !FateReset did not restore any Fate Points!");
+	}
 }
 });
