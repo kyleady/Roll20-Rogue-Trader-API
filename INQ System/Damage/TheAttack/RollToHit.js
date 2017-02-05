b63275de16d3c98368e84483bef9347857ed2dac
Kyle Ady <kyledavidady@gmail.com>
2017-02-05 01:49:53 -0800

    Adding functionality for skill subgroups with Psychic Attacks

--- INQ System/Damage/TheAttack/RollToHit.js
+++ INQ System/Damage/TheAttack/RollToHit.js
@@ -83,11 +83,36 @@ INQAttack.skillBonus = function(){
   if(INQAttack.inqweapon.FocusSkill){
     //check the character for the skill
     var skill = INQAttack.inqcharacter.has(INQAttack.inqweapon.FocusSkill, "Skills");
-    if(skill){
-      bonus = skill.Bonus;
+    if(!skill){
+      bonus = -20;
+    } else if(skill.length > 0){
+      //did the user provide a subgroup?
+      if(INQAttack.inqweapon.FocusSkillGroup){
+        //does the character have the given subgroup?
+        var regex = "^\\s*";
+        regex += INQAttack.inqweapon.FocusSkillGroup.replace(/(-|\s+)/g,"(-|\\s+)");
+        regex += "\\s*$";
+        var re = RegExp(regex, "i");
+        var matchingSubgroup = false;
+        var subgroupModifier = -20;
+        _.each(skill, function(subgroup){
+          if(re.test(subgroup.Name) || /^\s*all\s*$/i.test(subgroup.Name)){
+            //overwrite the subgroup's modifier if it is better
+            if(subgroup.Bonus > subgroupModifier){
+              subgroupModifier = subgroup.Bonus;
+            }
+          }
+        });
+        //if the character does not have a matching subgroup, give them a flat -20 modifier
+        bonus = subgroupModifier;
+      } else {
+        whisper("Psychic Power did not provide a skill group.");
+        bonus = -20;
+      }
+    //the skill was found, and there is no need to match subgroups
     } else {
       //the skill was not found
-      bonus = -20;
+      bonus = skill.Bonus;
     }
   }
   return bonus;
