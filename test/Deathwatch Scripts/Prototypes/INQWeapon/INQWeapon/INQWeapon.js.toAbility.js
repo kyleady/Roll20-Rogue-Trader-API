2c1ddc08164019cc29af01f70c29ada64d28fbbf
Kyle Ady <kyledavidady@gmail.com>
2017-12-11 15:39:52 -0800

    Modified INQWeapon.prototype.toAbility() so that it properly prepares a weapon for the new !useWeapon{} syntax.

--- test/Deathwatch Scripts/Prototypes/INQWeapon/INQWeapon/INQWeapon.js.toAbility.js
+++ test/Deathwatch Scripts/Prototypes/INQWeapon/INQWeapon/INQWeapon.js.toAbility.js
@@ -15,7 +15,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Knife(D10 R; Pen 0; Primitive, Balanced)');
     var action = inqweapon.toAbility(inqcharacter, {}, false);
 
-    expect(action).to.equal('!useWeapon Knife{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack|Swift Attack}"}');
+    expect(action).to.equal('!useWeapon Knife{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack|Swift Attack}","target":"@{target|token_id}"}');
   });
   it('should default to no special ammunition if no ammo is given', function(){
 		Campaign().MOCK20reset();
@@ -29,7 +29,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Knife(D10 R; Pen 0; Primitive, Balanced)');
     var action = inqweapon.toAbility(inqcharacter, {});
 
-    expect(action).to.equal('!useWeapon Knife{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack|Swift Attack}"}');
+    expect(action).to.equal('!useWeapon Knife{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack|Swift Attack}","target":"@{target|token_id}"}');
   });
   it('should default to no options if no option object is given', function(){
 		Campaign().MOCK20reset();
@@ -43,7 +43,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Knife(D10 R; Pen 0; Primitive, Balanced)');
     var action = inqweapon.toAbility(inqcharacter);
 
-    expect(action).to.equal('!useWeapon Knife{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack|Swift Attack}"}');
+    expect(action).to.equal('!useWeapon Knife{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack|Swift Attack}","target":"@{target|token_id}"}');
   });
   it('should ignore features that require a character if no character is given', function(){
 		Campaign().MOCK20reset();
@@ -55,7 +55,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Knife(D10 R; Pen 0; Primitive, Balanced)');
     var action = inqweapon.toAbility();
 
-    expect(action).to.equal('!useWeapon Knife{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack}"}');
+    expect(action).to.equal('!useWeapon Knife{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack}","target":"@{target|token_id}"}');
   });
   it('should not include the RoF option of All Out Attack if the weapon is not a Melee weapon', function(){
 		Campaign().MOCK20reset();
@@ -67,7 +67,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Meltagun(Basic; 10m; S/-/-; 10D10 E; Pen 10; Melta)');
     var action = inqweapon.toAbility();
 
-    expect(action).to.equal('!useWeapon Meltagun{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot}"}');
+    expect(action).to.equal('!useWeapon Meltagun{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot}","target":"@{target|token_id}"}');
   });
   it('should not include the RoF option of Called Shot if the weapon cannot fire on Single', function(){
 		Campaign().MOCK20reset();
@@ -79,7 +79,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Storm Bolter(Basic; 150m; -/3/6; 2D10 X; Pen 5; Tearing, Storm)');
     var action = inqweapon.toAbility();
 
-    expect(action).to.equal('!useWeapon Storm Bolter{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Semi Auto(3)|Full Auto(6)}"}');
+    expect(action).to.equal('!useWeapon Storm Bolter{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Semi Auto(3)|Full Auto(6)}","target":"@{target|token_id}"}');
   });
   it('should not include a roll20 choice for RoF, if there is only one choice', function(){
 		Campaign().MOCK20reset();
@@ -91,7 +91,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Heavy Bolter(Heavy; 300m; -/-/6; 3D10 X; Pen 5; Tearing)');
     var action = inqweapon.toAbility();
 
-    expect(action).to.equal('!useWeapon Heavy Bolter{"Modifier":"?{Modifier|0}","RoF":"Full Auto(6)"}');
+    expect(action).to.equal('!useWeapon Heavy Bolter{"modifiers":"?{Modifier|0}","RoF":"Full Auto(6)","target":"@{target|token_id}"}');
   });
   it('should not include the Roll20 choice of Called Shot if the weapon is Psychic', function(){
 		Campaign().MOCK20reset();
@@ -103,7 +103,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Bolt of Change(Psychic; PR x 10m; PRD10 E; Pen 2PR)');
     var action = inqweapon.toAbility();
 
-    expect(action).to.equal('!useWeapon Bolt of Change{"Modifier":"?{Modifier|0}","RoF":"Single"}');
+    expect(action).to.equal('!useWeapon Bolt of Change{"modifiers":"?{Modifier|0}","RoF":"Single","target":"@{target|token_id}"}');
   });
   it('should include the Rate of Fire Formula in Parentheses if firing on Semi or Full Auto', function(){
 		Campaign().MOCK20reset();
@@ -115,7 +115,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Fire Barrage(Psychic; PR x 5m; S/PR/2PR; D10 + PR E; Pen PR; Fire)');
     var action = inqweapon.toAbility();
 
-    expect(action).to.equal('!useWeapon Fire Barrage{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Semi Auto(PR)|Full Auto(2PR)}"}');
+    expect(action).to.equal('!useWeapon Fire Barrage{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Semi Auto(PR)|Full Auto(2PR)}","target":"@{target|token_id}"}');
   });
   it('should include the Swift Attack option if the weapon is a Melee Weapon and the Character has the Swift Attack Talent', function(){
 		Campaign().MOCK20reset();
@@ -130,18 +130,18 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var action;
 
     action = rangedweapon.toAbility(inqcharacter);
-    expect(action).to.equal('!useWeapon Meltagun{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot}"}');
+    expect(action).to.equal('!useWeapon Meltagun{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot}","target":"@{target|token_id}"}');
 
     action = meleeweapon.toAbility(inqcharacter);
-    expect(action).to.equal('!useWeapon Knife{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack}"}');
+    expect(action).to.equal('!useWeapon Knife{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack}","target":"@{target|token_id}"}');
 
     inqcharacter.List.Talents.push(new INQLink('Swift Attack'));
 
     action = rangedweapon.toAbility(inqcharacter);
-    expect(action).to.equal('!useWeapon Meltagun{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot}"}');
+    expect(action).to.equal('!useWeapon Meltagun{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot}","target":"@{target|token_id}"}');
 
     action = meleeweapon.toAbility(inqcharacter);
-    expect(action).to.equal('!useWeapon Knife{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack|Swift Attack}"}');
+    expect(action).to.equal('!useWeapon Knife{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack|Swift Attack}","target":"@{target|token_id}"}');
   });
   it('should include the Lightning Attack option if the weapon is a Melee Weapon and the Character has the Lightning Attack Talent', function(){
 		Campaign().MOCK20reset();
@@ -156,19 +156,19 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var action;
 
     action = rangedweapon.toAbility(inqcharacter);
-    expect(action).to.equal('!useWeapon Meltagun{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot}"}');
+    expect(action).to.equal('!useWeapon Meltagun{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot}","target":"@{target|token_id}"}');
 
     action = meleeweapon.toAbility(inqcharacter);
-    expect(action).to.equal('!useWeapon Knife{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack}"}');
+    expect(action).to.equal('!useWeapon Knife{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack}","target":"@{target|token_id}"}');
 
     inqcharacter.List.Talents.push(new INQLink('Swift Attack'));
     inqcharacter.List.Talents.push(new INQLink('Lightning Attack'));
 
     action = rangedweapon.toAbility(inqcharacter);
-    expect(action).to.equal('!useWeapon Meltagun{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot}"}');
+    expect(action).to.equal('!useWeapon Meltagun{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot}","target":"@{target|token_id}"}');
 
     action = meleeweapon.toAbility(inqcharacter);
-    expect(action).to.equal('!useWeapon Knife{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack|Swift Attack|Lightning Attack}"}');
+    expect(action).to.equal('!useWeapon Knife{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack|Swift Attack|Lightning Attack}","target":"@{target|token_id}"}');
   });
   it('should not include the RoF option or the Modifier option if the weapon has the Spray quality and is not Psychic', function(){
 		Campaign().MOCK20reset();
@@ -184,10 +184,10 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var action;
 
     action = psychicweapon.toAbility(inqcharacter);
-    expect(action).to.equal('!useWeapon Avenger{"Modifier":"?{Modifier|0}","RoF":"Single","EffectivePR":"?{Effective PsyRating|@{Psychic Character|PR}}"}');
+    expect(action).to.equal('!useWeapon Avenger{"modifiers":"?{Modifier|0}","RoF":"Single","BonusPR":"?{Bonus Psy Rating|0}","target":"@{target|token_id}"}');
 
     action = rangedweapon.toAbility(inqcharacter);
-    expect(action).to.equal('!useWeapon Flamer{}');
+    expect(action).to.equal('!useWeapon Flamer{"RoF":"Single","target":"@{target|token_id}"}');
   });
   it('should include an EffectivePR option with a roll20 choice if the weapon is psychic', function(){
 		Campaign().MOCK20reset();
@@ -201,7 +201,37 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var psychicweapon = new INQWeapon('Smite(Psychic; PR x 10m; S/-/-; PRD10 E; Pen PR; Blast[PR])');
 
     var action = psychicweapon.toAbility(inqcharacter);
-    expect(action).to.equal('!useWeapon Smite{"Modifier":"?{Modifier|0}","RoF":"Single","EffectivePR":"?{Effective PsyRating|@{Psychic Character|PR}}"}');
+    expect(action).to.equal('!useWeapon Smite{"modifiers":"?{Modifier|0}","RoF":"Single","BonusPR":"?{Bonus Psy Rating|0}","target":"@{target|token_id}"}');
+  });
+	it('should include a FocusStrength choice if the character has Unbound Psyker', function(){
+		Campaign().MOCK20reset();
+		var filePath = path.join(__dirname, '..', '..', '..', '..', '..', 'INQTotal.js');
+		var MyScript = fs.readFileSync(filePath, 'utf8');
+		eval(MyScript);
+		MOCK20endOfLastScript();
+
+    var inqcharacter = new INQCharacter();
+		inqcharacter.List.Traits.push(new INQLink('Unbound Psyker'));
+    inqcharacter.Name = 'Psychic Character';
+    var psychicweapon = new INQWeapon('Smite(Psychic; PR x 10m; S/-/-; PRD10 E; Pen PR; Blast[PR])');
+
+    var action = psychicweapon.toAbility(inqcharacter);
+    expect(action).to.equal('!useWeapon Smite{"modifiers":"?{Modifier|0}","RoF":"Single","FocusStrength":"?{Focus Strength|Unfettered|Push|True}","BonusPR":"?{Bonus Psy Rating|0}","target":"@{target|token_id}"}');
+  });
+	it('should include a FocusStrength choice if the character has Bound Psyker', function(){
+		Campaign().MOCK20reset();
+		var filePath = path.join(__dirname, '..', '..', '..', '..', '..', 'INQTotal.js');
+		var MyScript = fs.readFileSync(filePath, 'utf8');
+		eval(MyScript);
+		MOCK20endOfLastScript();
+
+    var inqcharacter = new INQCharacter();
+		inqcharacter.List.Traits.push(new INQLink('Bound Psyker'));
+    inqcharacter.Name = 'Psychic Character';
+    var psychicweapon = new INQWeapon('Smite(Psychic; PR x 10m; S/-/-; PRD10 E; Pen PR; Blast[PR])');
+
+    var action = psychicweapon.toAbility(inqcharacter);
+    expect(action).to.equal('!useWeapon Smite{"modifiers":"?{Modifier|0}","RoF":"Single","FocusStrength":"?{Focus Strength|Fettered|Unfettered|Push}","BonusPR":"?{Bonus Psy Rating|0}","target":"@{target|token_id}"}');
   });
   it('should include an ammo option if an array of ammunition is given', function(){
 		Campaign().MOCK20reset();
@@ -214,7 +244,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Bolter(Basic; 150m; S/3/-; 2D10 X; Pen 4; Tearing)');
 
     var action = inqweapon.toAbility(inqcharacter, {}, ['', 'Hellfire', 'Kraken Rounds']);
-    expect(action).to.equal('!useWeapon Bolter{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|Semi Auto(3)}","Ammo":"?{Special Ammunition||Hellfire|Kraken Rounds}"}');
+    expect(action).to.equal('!useWeapon Bolter{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|Semi Auto(3)}","Ammo":"?{Special Ammunition||Hellfire|Kraken Rounds}","target":"@{target|token_id}"}');
   });
   it('should not include a roll20 choice in the ammo option if the array has a size of 1', function(){
 		Campaign().MOCK20reset();
@@ -227,7 +257,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Bolter(Basic; 150m; S/3/-; 2D10 X; Pen 4; Tearing)');
 
     var action = inqweapon.toAbility(inqcharacter, {}, ['Vengeance Rounds']);
-    expect(action).to.equal('!useWeapon Bolter{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|Semi Auto(3)}","Ammo":"Vengeance Rounds"}');
+    expect(action).to.equal('!useWeapon Bolter{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|Semi Auto(3)}","Ammo":"Vengeance Rounds","target":"@{target|token_id}"}');
   });
   it('should include a roll20 choice in Special if the weapon can use Maximal', function(){
 		Campaign().MOCK20reset();
@@ -240,7 +270,20 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Plasma Pistol(Pistol; 30m; S/2/-; 6D10 E; Pen 8; Maximal, Overheats)');
 
     var action = inqweapon.toAbility(inqcharacter);
-    expect(action).to.equal('!useWeapon Plasma Pistol{"Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|Semi Auto(2)}","Special":"?{Fire on Maximal?|Use Maximal|}"}');
+    expect(action).to.equal('!useWeapon Plasma Pistol{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|Semi Auto(2)}","Special":"?{Fire on Maximal?|Use Maximal|}","target":"@{target|token_id}"}');
+  });
+	it('should include a roll20 choice in Special if the weapon can use Overcharge', function(){
+		Campaign().MOCK20reset();
+		var filePath = path.join(__dirname, '..', '..', '..', '..', '..', 'INQTotal.js');
+		var MyScript = fs.readFileSync(filePath, 'utf8');
+		eval(MyScript);
+		MOCK20endOfLastScript();
+
+    var inqcharacter = new INQCharacter();
+    var inqweapon = new INQWeapon('Sonic Boom(Pistol; 30m; S/2/-; 6D10 E; Pen 8; Overcharge)');
+
+    var action = inqweapon.toAbility(inqcharacter);
+    expect(action).to.equal('!useWeapon Sonic Boom{"modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|Semi Auto(2)}","Special":"?{Fire on Overcharge?|Use Overcharge|}","target":"@{target|token_id}"}');
   });
   it('should record the weapon in options.custum if option.custom was true', function(){
 		Campaign().MOCK20reset();
@@ -253,7 +296,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Scything Talons (D10+2 R; Pen 3)');
 
     var action = inqweapon.toAbility(inqcharacter, {custom: true});
-    expect(action).to.equal('!useWeapon Scything Talons{"custom":"Scything Talons (Melee; D10 + 2 R; Pen 3)","Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack}"}');
+    expect(action).to.equal('!useWeapon Scything Talons{"custom":"Scything Talons (Melee; D10 + 2 R; Pen 3)","modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|All Out Attack}","target":"@{target|token_id}"}');
   });
 	it('should allow you to record other details of the Weapon in the options', function(){
 		Campaign().MOCK20reset();
@@ -266,7 +309,7 @@ describe('INQWeapon.prototype.toAbility()', function() {
     var inqweapon = new INQWeapon('Plasma Pistol(Pistol; 30m; S/2/-; 6D10 E; Pen 8; Maximal, Overheats)');
 
     var action = inqweapon.toAbility(inqcharacter, {Range: '100'});
-    expect(action).to.equal('!useWeapon Plasma Pistol{"Range":"100","Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|Semi Auto(2)}","Special":"?{Fire on Maximal?|Use Maximal|}"}');
+    expect(action).to.equal('!useWeapon Plasma Pistol{"Range":"100","modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|Semi Auto(2)}","Special":"?{Fire on Maximal?|Use Maximal|}","target":"@{target|token_id}"}');
   });
 	it('should account for changes you made in the options', function(){
 		Campaign().MOCK20reset();
@@ -281,9 +324,9 @@ describe('INQWeapon.prototype.toAbility()', function() {
 		var action;
 
     action = plasmaweapon.toAbility(inqcharacter, {Semi: '3', Full: '5'});
-    expect(action).to.equal('!useWeapon Plasma Pistol{"Semi":"3","Full":"5","Modifier":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|Semi Auto(3)|Full Auto(5)}","Special":"?{Fire on Maximal?|Use Maximal|}"}');
+    expect(action).to.equal('!useWeapon Plasma Pistol{"Semi":"3","Full":"5","modifiers":"?{Modifier|0}","RoF":"?{Rate of Fire|Single|Called Shot|Semi Auto(3)|Full Auto(5)}","Special":"?{Fire on Maximal?|Use Maximal|}","target":"@{target|token_id}"}');
 
 		action = meltaweapon.toAbility(inqcharacter, {Special: 'Overheats, Spray'});
-    expect(action).to.equal('!useWeapon Infernus Pistol{"Special":"Overheats, Spray"}');
+    expect(action).to.equal('!useWeapon Infernus Pistol{"Special":"Overheats, Spray","RoF":"Single","target":"@{target|token_id}"}');
   });
 });
