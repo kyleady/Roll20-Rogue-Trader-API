c67295b42beb148223bf6d54d4098d4e29e25e7b
Kyle Ady <kyledavidady@gmail.com>
2018-01-02 09:13:54 -0800

    Added tests for lastWatchWave(). Changed lastWatchWave second match to be the percent chance of upgrading a group of troops instead of the one in ten chance of them not upgrading. Changed the API command to require the % symbol with the percent chance.

--- INQTotal.js
+++ INQTotal.js
@@ -1474,18 +1474,18 @@ function lastWatchWave (matches, msg) {
   var Troops = Number(matches[1]);
   var Elite = 0;
   var Master = 0;
-  var Chance = matches[2] || 4;
+  var Chance = matches[2] || 60;
   Chance = Number(Chance);
   var MasterPotential = Math.floor(Troops / 16);
   for (var i = 0; i < MasterPotential; i++) {
-    if (randomInteger(10) >= Chance) {
+    if (randomInteger(100) <= Chance) {
       Troops -= 16;
       Master++;
     }
   }
   var ElitePotential = Math.floor(Troops / 4);
   for (var i = 0; i < ElitePotential; i++) {
-    if (randomInteger(10) >= Chance) {
+    if (randomInteger(100) <= Chance) {
       Troops -= 4;
       Elite++;
     }
@@ -1494,12 +1494,12 @@ function lastWatchWave (matches, msg) {
   var output = '';
   if (Master) output += 'Master: ' + Master + ", ";
   if (Elite) output += 'Elite: ' + Elite + ", ";
-  if(Troop) output += 'Horde: ' + (Troop * 5);
-  whisper(output.replace(/,$/, ''));
+  if(Troops) output += 'Horde: ' + (Troops * 5);
+  whisper(output.replace(/,\s*$/, ''));
 }
 
 on("ready", function(){
-  CentralInput.addCMD(/^!\s*last\s*watch\s*wave\s*(\d+)\s*(\d*)\s*$/i, lastWatchWave);
+  CentralInput.addCMD(/^!\s*last\s*watch\s*wave\s*(\d+)\s*(?:(\d+)%)?\s*$/i, lastWatchWave);
 });
 //allows the gm to create a new roll20 character sheet that represents a brand
 //new character.
--- Scripts/Deathwatch Scripts/API Commands/Setup/LastWatchWave.js
+++ Scripts/Deathwatch Scripts/API Commands/Setup/LastWatchWave.js
@@ -2,18 +2,18 @@ function lastWatchWave (matches, msg) {
   var Troops = Number(matches[1]);
   var Elite = 0;
   var Master = 0;
-  var Chance = matches[2] || 4;
+  var Chance = matches[2] || 60;
   Chance = Number(Chance);
   var MasterPotential = Math.floor(Troops / 16);
   for (var i = 0; i < MasterPotential; i++) {
-    if (randomInteger(10) >= Chance) {
+    if (randomInteger(100) <= Chance) {
       Troops -= 16;
       Master++;
     }
   }
   var ElitePotential = Math.floor(Troops / 4);
   for (var i = 0; i < ElitePotential; i++) {
-    if (randomInteger(10) >= Chance) {
+    if (randomInteger(100) <= Chance) {
       Troops -= 4;
       Elite++;
     }
@@ -22,10 +22,10 @@ function lastWatchWave (matches, msg) {
   var output = '';
   if (Master) output += 'Master: ' + Master + ", ";
   if (Elite) output += 'Elite: ' + Elite + ", ";
-  if(Troop) output += 'Horde: ' + (Troop * 5);
-  whisper(output.replace(/,$/, ''));
+  if(Troops) output += 'Horde: ' + (Troops * 5);
+  whisper(output.replace(/,\s*$/, ''));
 }
 
 on("ready", function(){
-  CentralInput.addCMD(/^!\s*last\s*watch\s*wave\s*(\d+)\s*(\d*)\s*$/i, lastWatchWave);
+  CentralInput.addCMD(/^!\s*last\s*watch\s*wave\s*(\d+)\s*(?:(\d+)%)?\s*$/i, lastWatchWave);
 });
