73b1ffe74191686694b70a0c14f823cf881bc374
Kyle Smith <kyledavidady@gmail.com>
2019-02-16 20:17:45 -0800

    Transfer excess damage to crits

--- INQTotal.js
+++ INQTotal.js
@@ -2067,6 +2067,35 @@ on("change:graphic:bar3_value", function(obj, prev) {
     }
   }
 });
+const INQMoveCriticalDamage = function(graphic) {
+  const characterid = graphic.get('represents');
+  if(!characterid) return;
+  const critical_obj = findObjs({
+    name: 'Critical',
+    _characterid: characterid,
+    _type: 'attribute'
+  })[0];
+
+  if(!critical_obj) return;
+  let wounds = Number(graphic.get('bar1_value'));
+  let critical = Number(graphic.get('bar3_value'));
+  const critical_max = Number(graphic.get('bar3_max'));
+  if(wounds < 0) {
+    critical += wounds;
+    wounds = 0;
+  } else if(critical > critical_max) {
+    wounds += critical - critical_max;
+    critical = critical_max;
+  } else {
+    return;
+  }
+
+  graphic.set('bar1_value', wounds);
+  graphic.set('bar3_value', critical);
+}
+
+on('change:graphic:bar3_value', INQMoveCriticalDamage);
+on('change:graphic:bar1_value', INQMoveCriticalDamage);
 on('ready', function() {
   INQTime.on('change:time', function(currTime, prevTime, dt) {
     var myPromise = new Promise(function(resolve) {
