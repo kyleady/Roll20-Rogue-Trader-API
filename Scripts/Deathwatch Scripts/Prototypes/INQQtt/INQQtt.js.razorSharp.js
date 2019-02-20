4f524c068cbb104a6a517bb978799a01db47e977
Kyle Smith <kyledavidady@gmail.com>
2019-02-19 20:40:23 -0800

    Fix Razor Sharp

--- Scripts/Deathwatch Scripts/Prototypes/INQQtt/INQQtt.js.razorSharp.js
+++ Scripts/Deathwatch Scripts/Prototypes/INQQtt/INQQtt.js.razorSharp.js
@@ -2,7 +2,7 @@ INQQtt.prototype.razorSharp = function(){
   var inqweapon = this.inquse.inqweapon;
   var successes = this.inquse.inqtest.Successes;
   if(!inqweapon.has('Razor Sharp')) return;
-  if(successes >= 2) return;
+  if(successes <= 2) return;
   log('Razor Sharp');
   inqweapon.Penetration.Multiplier *= 2;
 }
